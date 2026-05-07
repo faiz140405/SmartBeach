47eea2f6ee31b99098484c90add766c80486bf52
Faiz Nizar Nu'aim <167897077+faiz140405@users.noreply.github.com>
2026-05-07 15:01:30 +0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,4 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  darkMode: "class",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
